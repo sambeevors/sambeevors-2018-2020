c7a902947579baef3740e68e86bd808c85264cf3
sambeevors <sam.beevors@parall.ax>
2018-11-28 11:47:49 +0000

    Added netlift contact form

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -11,4 +11,27 @@ $.ready(() => {
 
   const header = new HeightGroup('.js-header-spacer')
   header.watchElements()
+
+  const subscribeBtn = $.qs('.js-subscribe')
+  const modalContainer = $.qs('.js-modal-container')
+
+  const toggleModal = () => {
+    modalContainer.classList.toggle('pointer-events-none')
+    modalContainer.classList.toggle('opacity-0')
+  }
+
+  $.on('click', subscribeBtn, e => {
+    e.preventDefault()
+    toggleModal()
+  })
+
+  $.on('click', modalContainer, '.js-close', e => {
+    e.preventDefault()
+    toggleModal()
+  })
+
+  $.on('click', modalContainer, e => {
+    e.preventDefault()
+    if (e.target === modalContainer) toggleModal()
+  })
 })
--- tailwind-config.js
+++ tailwind-config.js
@@ -871,6 +871,22 @@ module.exports = {
     require('tailwindcss/plugins/container')({
       // center: true,
       // padding: '1rem',
+    }),
+    require('tailwindcss-alpha')({
+      modules: {
+        backgroundColors: true
+      },
+      alpha: {
+        '10': 0.1,
+        '20': 0.2,
+        '30': 0.3,
+        '40': 0.4,
+        '50': 0.5,
+        '60': 0.6,
+        '70': 0.7,
+        '80': 0.8,
+        '90': 0.9
+      }
     })
   ],
 
