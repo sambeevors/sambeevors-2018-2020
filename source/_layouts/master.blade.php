0180d254c6b6acb05b2cfa907b1c2adf8c470edc
sambeevors <sam.beevors@parall.ax>
2018-12-03 11:16:44 +0000

    Remove empty desc tag

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -60,7 +60,6 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
             <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <meta name="description" content="" />
 
             @foreach ($hashed_files[0] as $key => $file)
 
