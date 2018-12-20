24778d43656953ebffbac6aba5d4c73959b4028a
Sam Beevors <sambeevors@googlemail.com>
2018-12-20 13:46:40 +0000

    Added esc to exit modals / nav (#28)

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -30,11 +30,19 @@ $.ready(() => {
 
   const $burger = $.qs('.js-burger')
   const $nav = $.qs('.js-nav')
-  if ($burger) {
+  if ($burger && $nav) {
     $burger.addEventListener('click', function (e) {
       e.preventDefault()
       $nav.classList.toggle('-active')
       $burger.classList.toggle('-active')
+    })
+
+    document.addEventListener('keydown', function (e) {
+      console.log(e)
+      if (e.key === 'Escape' && $nav.classList.contains('-active')) {
+        $nav.classList.remove('-active')
+        $burger.classList.remove('-active')
+      }
     })
   }
 
